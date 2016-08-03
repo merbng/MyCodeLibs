6f4dc4be64bd445e022eb702528c80b8af2b670a
merbng <1146797082@qq.com>
2016-08-03 23:06:27 +0800

    删无用

--- app/src/main/java/com/app/merbng/mycodelibs/activitys/GlideActivity.java
+++ app/src/main/java/com/app/merbng/mycodelibs/activitys/GlideActivity.java
@@ -40,7 +40,7 @@ public class GlideActivity extends BaseActivity {
     }
 
     private ArrayList<TutorialItem> getTutorialItems(Context context) {
-        TutorialItem tutorialItem1 = new TutorialItem(R.string.slide_1_african_story_books, R.string.slide_1_african_story_books,
+        TutorialItem tutorialItem1 = new TutorialItem(R.string.slide_1_african_story_books_subtitle, R.string.slide_1_african_story_books,
                 R.color.slide_1, R.drawable.tut_page_1_front,  R.drawable.tut_page_1_background);
 
         TutorialItem tutorialItem2 = new TutorialItem(R.string.slide_2_volunteer_professionals, R.string.slide_2_volunteer_professionals_subtitle,
