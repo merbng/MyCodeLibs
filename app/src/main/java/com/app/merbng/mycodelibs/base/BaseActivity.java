e4696efbac8cd598165b3f7513e216e847d0e516
merbng <lyp82nlf>
2016-08-29 17:55:47 +0800

    +

--- app/src/main/java/com/app/merbng/mycodelibs/base/BaseActivity.java
+++ app/src/main/java/com/app/merbng/mycodelibs/base/BaseActivity.java
@@ -1,16 +1,22 @@
 package com.app.merbng.mycodelibs.base;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.KeyEvent;
+import android.widget.Toast;
 
 import com.app.merbng.mycodelibs.MyCodeLibApplication;
 import com.app.merbng.mycodelibs.activitys.MainActivity;
+import com.app.merbng.mycodelibs.interfaces.BaseActivityInterface;
 import com.sdsmdg.tastytoast.TastyToast;
 import com.zhy.changeskin.base.BaseSkinActivity;
 
+import java.lang.ref.WeakReference;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -21,12 +27,14 @@ import java.util.List;
  * @email maxl@dxyer.com
  * @date 2014-12-18
  */
-public class BaseActivity extends BaseSkinActivity {
-
+public class BaseActivity extends BaseSkinActivity implements BaseActivityInterface {
+    private final int CODE_MSG = 3721;// 发送消息
     protected Context mContext;
     protected Activity thisActivity;
     private List<Activity> closeActivitiesForChangeLoginUser = new LinkedList<Activity>();
     public ProgressDialog mProgressDialog;
+
+    protected MyHandler myHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +47,7 @@ public class BaseActivity extends BaseSkinActivity {
         mProgressDialog.setMax(100);
         mProgressDialog.setCancelable(true);
         mProgressDialog.setCanceledOnTouchOutside(true);
+        myHandler = new MyHandler(this);
     }
 
     /**
@@ -61,9 +70,20 @@ public class BaseActivity extends BaseSkinActivity {
         }
     }
 
-    protected void show(String message) {
-//        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
-        TastyToast.makeText(mContext,message,TastyToast.LENGTH_SHORT,TastyToast.INFO);
+    /**
+     * 弹出Toath
+     *
+     * @param str
+     */
+    public void show(String str) {
+        try {
+            Message message = Message.obtain(myHandler);
+            message.what = CODE_MSG;
+            message.obj = str;
+            message.sendToTarget();
+        } catch (Exception e) {
+            Toast.makeText(mContext, str, Toast.LENGTH_SHORT).show();
+        }
     }
 
     // 按两次返回键退出程序
@@ -87,7 +107,51 @@ public class BaseActivity extends BaseSkinActivity {
     }
 
     @Override
+    public void hand_message(Message msg) {
+
+    }
+
+    /**
+     * 发送Message
+     */
+    public void sendMessage(int what, Object obj, int arg1, int arg2) {
+        Message message = Message.obtain(myHandler);
+        message.what = what;
+        message.obj = obj;
+        message.arg1 = arg1;
+        message.arg2 = arg2;
+        message.sendToTarget();
+    }
+
+    @SuppressLint("HandlerLeak")
+    public class MyHandler extends Handler {
+        WeakReference<BaseActivity> mFragment;
+
+        MyHandler(BaseActivity acitivity) {
+            mFragment = new WeakReference<BaseActivity>(acitivity);
+        }
+
+        @Override
+        public void handleMessage(android.os.Message msg) {
+            if (msg.what == CODE_MSG) {
+                String msg_tis = (String) msg.obj;
+                if (null == msg_tis || msg_tis.equals("")) {
+                    Toast.makeText(mContext, null, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(mContext, msg_tis, Toast.LENGTH_SHORT).show();
+                }
+            }
+            hand_message(msg);
+        }
+
+    }
+
+    @Override
     protected void onDestroy() {
         super.onDestroy();
+        if (myHandler != null && myHandler.hasMessages(CODE_MSG)) {
+            myHandler.removeMessages(CODE_MSG);
+        }
+        MyCodeLibApplication.getmInstance().removeActivity(thisActivity);
     }
 }
