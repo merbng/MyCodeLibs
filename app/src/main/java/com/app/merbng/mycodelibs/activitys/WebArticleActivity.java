aa1ec9e90e3a1af97de03753e199552e29612d43
Merbng <1146797082@qq.com>
2017-03-13 16:28:50 +0800

    网页文章：支持微信和支付宝两种主流支付的集成库， 两行代码实现微信支付， 三行代码实现支付宝支付

--- app/src/main/java/com/app/merbng/mycodelibs/activitys/WebArticleActivity.java
+++ app/src/main/java/com/app/merbng/mycodelibs/activitys/WebArticleActivity.java
@@ -11,9 +11,13 @@ import com.app.merbng.mycodelibs.R;
 import com.app.merbng.mycodelibs.base.BaseActivity;
 import com.app.merbng.mycodelibs.utils.IntentUtils;
 
+/**
+ * 网页文章
+ */
 public class WebArticleActivity extends BaseActivity {
 //    一个 TextView 完成显示全文与隐藏功能
     private static final String text_view_show_hide = "http://p.codekk.com/detail/Android/timqi/CollapsibleTextView";
+    private static final String text_pay_for_zhifubao_weixin = "http://p.codekk.com/detail/Android/mayubao/Android-Pay";
 
 
     @Override
@@ -25,6 +29,9 @@ public class WebArticleActivity extends BaseActivity {
         switch (view.getId()) {
             case R.id.tv_article_CollapsibleTextView:
                 startActivity(IntentUtils.startWebView(mContext, text_view_show_hide, "一个 TextView 完成显示全文与隐藏功能"));
+                break;
+            case R.id.tv_pay_for_zhifubao_weixin:
+                startActivity(IntentUtils.startWebView(mContext, text_pay_for_zhifubao_weixin, "支持微信和支付宝两种主流支付的集成库， 两行代码实现微信支付， 三行代码实现支付宝支付"));
                 break;
         }
     }
