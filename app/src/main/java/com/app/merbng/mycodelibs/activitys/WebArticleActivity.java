7de532987fdc98867ed42a6b62cb7a66af003b4f
Merbng <1146797082@qq.com>
2017-03-15 16:04:10 +0800

    IKNinePhotoView 是一个开源的 Android 九宫格控件，可以自适应宽高主要用于满足九宫格图片展示器及选择器的需求。

--- app/src/main/java/com/app/merbng/mycodelibs/activitys/WebArticleActivity.java
+++ app/src/main/java/com/app/merbng/mycodelibs/activitys/WebArticleActivity.java
@@ -18,6 +18,7 @@ public class WebArticleActivity extends BaseActivity {
 //    一个 TextView 完成显示全文与隐藏功能
     private static final String text_view_show_hide = "http://p.codekk.com/detail/Android/timqi/CollapsibleTextView";
     private static final String text_pay_for_zhifubao_weixin = "http://p.codekk.com/detail/Android/mayubao/Android-Pay";
+    private static final String text_nine_phoneView = "http://p.codekk.com/detail/Android/Idtk/IKNinePhotoView";
 
 
     @Override
@@ -32,6 +33,9 @@ public class WebArticleActivity extends BaseActivity {
                 break;
             case R.id.tv_pay_for_zhifubao_weixin:
                 startActivity(IntentUtils.startWebView(mContext, text_pay_for_zhifubao_weixin, "支持微信和支付宝两种主流支付的集成库， 两行代码实现微信支付， 三行代码实现支付宝支付"));
+                break;
+            case R.id.tv_nine_phoneView:
+                startActivity(IntentUtils.startWebView(mContext, text_nine_phoneView, "IKNinePhotoView 是一个开源的 Android 九宫格控件，可以自适应宽高主要用于满足九宫格图片展示器及选择器的需求。"));
                 break;
         }
     }
